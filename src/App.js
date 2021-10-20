6e2ba80f5e46aaea877a5b91a18cda041d39f043
George Wilkins <gwilki2@gmail.com>
2021-10-19 19:27:22 -0500

    updated logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import nssLogo from './assets/_images/nssLogoTransparent.png'
+import styles from './App.module.scss'
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Sarcoidosis4TheArts.com</h1>
-    </div>
+      
+      <div className={styles.navbar}>
+        <h1>Sarcoidosis4TheArts.org</h1>
+        <img src={nssLogo} alt="Logo" />
+      </div>
+    </div> 
   );
 }
 
